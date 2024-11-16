2e6162cfb016ab3ce5bf96403afc54a853f2eca8
Oliver Castillo <ocastilt@nttdata.com>
2024-11-16 13:41:00 -0500

    chore: initial commit

---
+++ src/main/java/com/bank/apigateway/BankApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bank.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApiGatewayApplication.class, args);
+	}
+
+}
